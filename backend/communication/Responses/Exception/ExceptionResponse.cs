23594ba5ca01e7b6b1782335a91acb94b1bb7947
Pedro-HenridS <pedrosouza9479@gmail.com>
2025-08-04 20:25:22 -0300

    feature:tratamento de erros e criação de filtro

---
+++ backend/communication/Responses/Exception/ExceptionResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace communication.Responses.Exception
+{
+    public class ExceptionResponse
+    {
+        public List<string> Errors { get; set; } = [];
+
+        public ExceptionResponse(string error)
+        {
+            Errors = [error];
+        }
+
+        public ExceptionResponse(List<string> errors)
+        {
+            Errors = errors;
+        }
+    }
+}
---
+++ backend/hometask.api/Filters/ExceptionFilters.cs
@@ -0,0 +1,32 @@
+﻿using communication.Responses.Exception;
+using Exception.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace hometask.api.Filters
+{
+    public class ExceptionFilters : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context) 
+        {
+        
+        }
+
+        private void HandleException(ExceptionContext context)
+        {
+            if (context.Exception is RegisterException RegisterException)
+            {
+                var errors = RegisterException.Errors;
+
+                context.HttpContext.Response.StatusCode =StatusCodes.Status400BadRequest;
+                context.Result = new ObjectResult(errors);
+            }
+        }
+
+        private void UnknowException(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult();
+        }
+    }
+}
