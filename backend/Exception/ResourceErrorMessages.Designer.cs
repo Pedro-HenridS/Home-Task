aaa7c25dcf07c4fa87e740551e59114a5fc895d8
Pedro-HenridS <pedrosouza9479@gmail.com>
2025-08-05 13:36:02 -0300

    Stage changes

--- backend/Exception/ResourceErrorMessages.Designer.cs
+++ backend/Exception/ResourceErrorMessages.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Exception {
+namespace exceptions {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Exception {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Exception.ResourceErrorMessages", typeof(ResourceErrorMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("exceptions.ResourceErrorMessages", typeof(ResourceErrorMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
