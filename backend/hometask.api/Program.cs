c32da867ad48981831356a62a1460523572e5125
Pedro-HenridS <pedrosouza9479@gmail.com>
2025-08-09 11:06:13 -0300

    Commit antes do merge

--- backend/hometask.api/Program.cs
+++ backend/hometask.api/Program.cs
@@ -1,3 +1,4 @@
+
 using application.Services.Account;
 using application.Services.Encrypt;
 using application.Services.Jwt;
@@ -12,7 +13,6 @@ using infra.Services.Encrypt;
 using infra.Services.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,15 +32,15 @@ builder.Services.AddControllers(options => {
 // validador
 builder.Services.AddScoped<RegisterUserValidator>();
 
+
 // services
-builder.Services.AddScoped<EmailAlreadyInUseService>();
+
 builder.Services.AddScoped<RegisterUserService>();
 builder.Services.AddScoped<PasswordHasherService>();
 builder.Services.AddScoped<VerifyHashService>();
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<FindAccountService>();
 
-// Injeção de Dependência
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 builder.Services.AddScoped<IUserRegisterRepository, UserRegisterRepository>();
