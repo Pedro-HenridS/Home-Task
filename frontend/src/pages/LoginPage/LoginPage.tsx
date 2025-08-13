9a32e4cb2e7080c9c374bbd806c86e3086d2f4e5
Pedro-Henrids <pedrosouza9479@gmail.com>
2025-08-13 14:49:16 -0300

    feat: refatorando a tela de login

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -2,16 +2,25 @@ import LoginForm from '../../components/forms/Login/LoginForm';
 import { useRef } from "react";
 import './LoginPage.scss';
 import type { IFormHandle } from '../../interfaces/IFormHandle';
+import brandingImg from '../../assets/branding.jpg';
 
 export default function LoginPage(){
 
     const formRef = useRef<IFormHandle>(null);
     
     return(
-        <section>
-            <h1>Login</h1>
-            <LoginForm ref={formRef}></LoginForm>
-            <button onClick={() =>  formRef.current?.submitForm() }>Entrar</button>
-        </section> 
+        <div className='body'>
+            <main className="main">
+                <div className='box'>
+                <h1>Login</h1>
+                <LoginForm ref={formRef}></LoginForm>
+                <button onClick={() =>  formRef.current?.submitForm() }>Entrar</button>
+                </div>
+                
+            </main>
+            <aside className="aside">
+                <img className="image" src={brandingImg} alt="Branding" />
+            </aside>
+        </div> 
     )
 }
